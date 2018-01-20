32c3d75a8874a4807798765c21b54467680a94d1
Neozaru <neozaru@mailoo.org>
2018-01-20 11:29:59 +0100

    Fixed build directory cleaning

--- buildISO.sh
+++ buildISO.sh
@@ -14,8 +14,8 @@ git submodule update --init --recursive
 
 echo 'Submodules updated!'
 
-rm -R $BUILD_DIRECTORY/ && mkdir -p $BUILD_DIRECTORY
-cp -R $ARCHISO_CONFIG_DIRECTORY $BUILD_DIRECTORY/moneroz
+rm -R $BUILD_DIRECTORY/ && mkdir -p $BUILD_DIRECTORY/moneroz
+cp -R $ARCHISO_CONFIG_DIRECTORY/* $BUILD_DIRECTORY/moneroz
 
 mkdir -p $TARGET_SYSTEM_ROOT/usr/bin
 #pushd monero-gui && ./build.sh; popd
