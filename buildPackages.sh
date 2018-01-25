ece6839fc186c66bf7d4bfd1ca6c5bd9b4c588c4
Neozaru <neozaru@mailoo.org>
2018-01-25 11:56:56 +0100

    Build Packages script to create proper dirs

--- buildPackages.sh
+++ buildPackages.sh
@@ -7,17 +7,13 @@ export MAKEFLAGS="-j $( grep -c ^processor /proc/cpuinfo )"
 
 
 package() {
-  pushd $1 && makepkg -f ; popd
+  pushd $1 && git clean -fd && makepkg -f ; popd
   cp $1/*.pkg.tar.xz $CUSTOM_REPOSITORY_LOCATON/
 }
 
-# TODO: Fix monero-git to not crash when repository already exists
-rm -Rf monero-git-packager/monero
 package monero-git-packager
-
-rm -Rf monero-gui-git-packager/monero-gui
 package monero-gui-git-packager
-
 package kovri-git-packager
 
+mkdir -p $CUSTOM_REPOSITORY_LOCATON
 pushd $CUSTOM_REPOSITORY_LOCATON && repo-remove moneroz.db.tar.gz ; repo-add moneroz.db.tar.gz *.pkg.tar.xz ; popd
