b98e32393fc4ebf8ce45341b40c5fc4c3a8c287a
Neozaru <neozaru@mailoo.org>
2018-01-31 19:34:30 +0100

    Stupid typo (and useful commit messages)

--- buildWithDocker.sh
+++ buildWithDocker.sh
@@ -7,4 +7,4 @@ docker build -t moneroz-builder -f $BASE_DIRECTORY/Dockerfile $BASE_DIRECTORY
 [ "$(docker ps -a | grep moneroz)" ] && docker rm moneroz ;
 # TODO: Hardcoded source/destination 
 docker run --privileged --name moneroz moneroz-builder && 
-  docker cp moneroz:/home/builduser/build/moneroz-0.0.1-x86_64.iso.iso $PWD
+  docker cp moneroz:/home/builduser/build/moneroz-0.0.1-x86_64.iso $PWD
