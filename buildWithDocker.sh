8675eef9c613bcf020a9295c82a7b1340b357021
Neozaru <neozaru@mailoo.org>
2018-02-01 14:48:20 +0100

    Monero Wallet desktop shortcut

--- buildWithDocker.sh
+++ buildWithDocker.sh
@@ -4,7 +4,7 @@ set -e
 BASE_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker build -t moneroz-builder -f $BASE_DIRECTORY/Dockerfile $BASE_DIRECTORY
-[ "$(docker ps -a | grep moneroz)" ] && docker rm moneroz ;
+[ "$(docker ps -a | grep moneroz)" ] && docker rm -f moneroz ;
 # TODO: Hardcoded source/destination 
 docker run --privileged --name moneroz moneroz-builder && 
   docker cp moneroz:/home/builduser/build/moneroz-0.0.1-x86_64.iso $PWD
